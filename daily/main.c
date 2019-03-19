91baed826f7ca4b6225e74d8c965d93f61708571
Rui Z <48302470+Rui-Zgo@users.noreply.github.com>
2019-03-19 16:53:12 +0800

    2019/3/18,(其他方法验证国二试题)2,,(acm)1

--- daily/main.c
+++ daily/main.c
@@ -1,14 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+void change(double *x1,double *y1,double *x2,double *y2)
+{
+    double t;
+    if(*x1 > *x2)
+    {
+        t=*x1;
+        *x1=*x2;
+        *x2=t;
+    }
+    if(*y1 > *y2)
+    {
+        t=*y1;
+        *y1=*y2;
+        *y2=t;
+    }
+}
 int main()
 {
-    int n,h,h1,h2,m,m1,m2,m3,s,s1,s2,s3;
-    while(~scanf("%d",&n)&&n)
-        while(n--)
-        {scanf("%d%d%d%d%d%d",&h1,&m1,&s1,&h2,&m2,&s2);
-        s=(s1+s1)%60;
-        m=(m1+m2)%60+(s1+s2)/60;
-        h=h1+h2+(m1+m2)/60;
-        printf("%d %d %d\n",h,m,s);
-        }
+    double A[8],x1,y1,x2,y2;
+    while(~scanf("%lf%lf%lf%lf%lf%lf%lf%lf",&A[0], &A[1],&A[2],&A[3],&A[4],&A[5],&A[6],&A[7]))
+    {
+        change(&A[0],&A[1],&A[2],&A[3]);
+        change(&A[4],&A[5],&A[6],&A[7]);
+        x1= A[0] > A[4] ? A[0] : A[4];
+         y1= A[1] > A[5] ? A[1] : A[5];
+        x2= A[2] < A[6] ? A[2] : A[6];
+       y2= A[3] < A[7] ? A[3] : A[7];
+         if((x2-x1)<0 || (y2-y1)<0)
+            printf("0.00\n");
+        else
+            printf("%.2lf\n",(x2-x1)*(y2-y1));
+    }
+    return 0;
 }
-
